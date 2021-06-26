3c7389377cc76a4c14cd5eaa8b829843481ff463
yatw <wongyatman487@gmail.com>
2021-06-26 11:10:23 -0700

    Update gitignore

--- app/src/main/java/com/prettypasswords/view/fragments/HomeFragment.kt
+++ app/src/main/java/com/prettypasswords/view/fragments/HomeFragment.kt
@@ -38,6 +38,7 @@ class HomeFragment : Fragment() {
 
     val tags = PrettyManager.cm!!.body.tags
     lateinit var tagAdapter: TagAdapter
+    lateinit val ViewModel: 
 
 
     val addTagReceiver = object : BroadcastReceiver() {
